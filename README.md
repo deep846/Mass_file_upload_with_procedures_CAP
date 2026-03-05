c6f2086af176bf959288c17cf188a534e7774fff
Deep Choudhury <sanandachoudhury69@gmail.com>
2026-03-05 11:56:17 +0000

    Introducing Procedures

--- README.md
+++ README.md
@@ -23,3 +23,73 @@ File or Folder | Purpose
 ## Learn More
 
 Learn more at https://cap.cloud.sap/docs/get-started/.
+
+
+
+<!-- Here is the tutorials for procedures -->
+
+first we need to do 
+
+ 1. CTRL + SHIFT + P (open command palet)
+ 2. create hana DB artifect
+ 3. create using the below template
+    ![alt text](image.png)
+ 4. then create a .hdiconfig file contains below details.
+
+    {
+  "file_suffixes": {
+    "csv": {
+      "plugin_name": "com.sap.hana.di.tabledata.source"
+    },
+    "hdbcalculationview": {
+      "plugin_name": "com.sap.hana.di.calculationview"
+    },
+    "hdbconstraint": {
+      "plugin_name": "com.sap.hana.di.constraint"
+    },
+    "hdbindex": {
+      "plugin_name": "com.sap.hana.di.index"
+    },
+    "hdbtable": {
+      "plugin_name": "com.sap.hana.di.table"
+    },
+    "hdbtabledata": {
+      "plugin_name": "com.sap.hana.di.tabledata"
+    },
+    "hdbview": {
+      "plugin_name": "com.sap.hana.di.view"
+    },
+    
+    "hdbprocedure": {
+      "plugin_name": "com.sap.hana.di.procedure"
+    }
+  }
+}
+
+5. some example logics written in the created file
+
+     await cds.run(`CALL "8CC23DED985F4A2F8B315F68F418EBE4"."pro"('${ID}','${Name}','${Roll}','${Class}')`)
+            // this one is also okay.
+            // await cds.run(
+            //     `CALL "8CC23DED985F4A2F8B315F68F418EBE4"."pro"(?,?,?,?)`,
+            //     [ID, Name, Roll, Class]
+            // );
+6. WRITE ON YOUR PROCEDURE FILE LIKE:
+
+    PROCEDURE "pro"(
+    IN id INTEGER,
+    IN Name NVARCHAR(100),
+    IN Roll INTEGER,
+    IN Class INTEGER 
+)
+   LANGUAGE SQLSCRIPT
+   SQL SECURITY INVOKER
+--    DEFAULT SCHEMA <default_schema_name>
+   AS
+BEGIN
+   /*************************************
+       Write your procedure logic
+   *************************************/
+   INSERT INTO "MY_STUDENTS_STUDENTS"(ID, NAME, ROLL, CLASS)
+          VALUES(:id, :Name, :Roll, :Class);
+END
--- srv/studentBuisnessLogic/studentBuisnesslogic.js
+++ srv/studentBuisnessLogic/studentBuisnesslogic.js
@@ -1,6 +1,6 @@
 const xlsx = require('xlsx');
 
-async function onReadCustomer(req,next){
+async function onReadCustomer(req, next) {
     const data = await next();
     console.log(data);
     console.log("Yaa hoooo!!!!!");
@@ -8,42 +8,69 @@ async function onReadCustomer(req,next){
 
 async function uploadStudentExcel(req) {
     console.log("File Upload Triggered");
-    const {students} = this.entities;
+    const { students } = this.entities;
 
-    try{
+    try {
         const filecontent = req.data.file.split(',')[1];
-        const binaryString = Buffer.from(filecontent, 'base64').toString('binary');
-        const bytes = new Uint8Array(binaryString.length);
 
-        for(let i=0; i<binaryString.length; i++){
-            bytes[i] = binaryString.charCodeAt(i);
-        }
+        // 1st way that we have done on 1st lession
+        // const binaryString = Buffer.from(filecontent, 'base64').toString('binary');
+        // const bytes = new Uint8Array(binaryString.length);
 
-        const workbook = xlsx.read(bytes.buffer, {type: 'array'});
+        // for(let i=0; i<binaryString.length; i++){
+        //     bytes[i] = binaryString.charCodeAt(i);
+        // }
+
+        // 2nd way we are doing it
+
+        const buffer = Buffer.from(filecontent, 'base64');
+
+
+
+        // 1st way to do it
+        // const workbook = xlsx.read(bytes.buffer, {type: 'array'});
+
+        // 2nd way to do it
+        const workbook = xlsx.read(buffer.buffer, { type: 'array' });
+
+
         const SheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[SheetName];
         const StudentsData = xlsx.utils.sheet_to_json(sheet);
 
-        console.log('parsed data:  ',StudentsData);
+        console.log('parsed data:  ', StudentsData);
 
-        const FormattedStudentsData = StudentsData.map(record=>({
+        const FormattedStudentsData = StudentsData.map(record => ({
             ...record
         }));
 
-        await INSERT.into(students).entries(FormattedStudentsData);
+        // 1st way to do it
+        // await INSERT.into(students).entries(FormattedStudentsData);
 
-        return {message: "File Uploaded successfully"};
+        // 2nd way to do it via procedure. MASS UPLOADING via procedure.
+        for (const student of FormattedStudentsData) {
+            const { ID, Name, Roll, Class } = student;
+            //this one is also okay 
+            await cds.run(`CALL "8CC23DED985F4A2F8B315F68F418EBE4"."pro"('${ID}','${Name}','${Roll}','${Class}')`)
+            // this one is also okay.
+            // await cds.run(
+            //     `CALL "8CC23DED985F4A2F8B315F68F418EBE4"."pro"(?,?,?,?)`,
+            //     [ID, Name, Roll, Class]
+            // );
+        }
+
+        return { message: "File Uploaded successfully" };
 
 
 
 
     }
-    catch(e){
-        console.log("Error During upload",e);
-        return req.error(400,'Invalid file content');
+    catch (e) {
+        console.log("Error During upload", e);
+        return req.error(400, 'Invalid file content');
 
     }
-    
+
 }
 
-module.exports = {onReadCustomer,uploadStudentExcel}
+module.exports = { onReadCustomer, uploadStudentExcel }
